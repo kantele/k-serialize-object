8075f97adae957cdb23e1c06a1d761054dbf0e9c
Nate Smith <nate@nateps.com>
2013-11-26 23:59:09 -0800

    initial implementation

---
+++ index.js
@@ -0,0 +1,55 @@
+exports.instance = serializeInstance;
+exports.value = serializeValue;
+
+function serializeInstance(instance) {
+  // Map each argument into its string representation
+  var args = [];
+  for (var i = arguments.length; i-- > 1;) {
+    var arg = serializeValue(arguments[i]);
+    args.unshift(arg);
+  }
+  // Remove trailing null values, assuming they are optional
+  for (var i = args.length; i--;) {
+    var arg = args[i];
+    if (arg !== 'void 0' && arg !== 'null') break;
+    args.pop();
+  }
+  return 'new ' + instance.module + '.' + instance.type + '(' + args.join(', ') + ')';
+}
+
+function serializeValue(input) {
+  if (input && input.serialize) {
+    return input.serialize();
+
+  } else if (typeof input == 'undefined') {
+    return 'void 0';
+
+  } else if (input === null) {
+    return 'null';
+
+  } else if (typeof input === 'string') {
+    return quoteString(input);
+
+  } else if (typeof input === 'number' || typeof input === 'boolean') {
+    return input + '';
+
+  } else if (Array.isArray(input)) {
+    var items = [];
+    for (var i = 0; i < input.length; i++) {
+      var value = serializeValue(input[i]);
+      items.push(value);
+    }
+    return '[' + items.join(', ') + ']';
+
+  } else if (typeof input == 'object') {
+    var items = [];
+    for (var key in input) {
+      var value = serializeValue(input[key]);
+      items.push(quoteString(key) + ': ' + value);
+    }
+    return '{' + items.join(', ') + '}';
+  }
+}
+function quoteString(value) {
+  return '\'' + value.replace(/'/g, '\\\'') + '\'';
+}
